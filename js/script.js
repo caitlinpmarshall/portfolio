43a9e9f5d1b755f9b1126bfd79238b696faf221f
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-08-01 20:38:00 -0400

    Completes sticky header and hover effects

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
 /** sticky header **/
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+const header = document.getElementById("myHeader");
+const sticky = header.offsetTop;
 
-// Get the header
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickIt() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 } 
+
+window.onscroll = function() {stickIt()};
